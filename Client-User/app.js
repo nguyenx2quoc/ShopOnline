6463a12ee2f9b9a1383e34a6602eb6d57e2e4a8e
HuuPhuoc95 <huuphuoc3304@gmail.com>
2017-01-02 11:25:13 +0700

    cap nhat giao dien

--- Client-User/app.js
+++ Client-User/app.js
@@ -11,22 +11,32 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('women', {
             url: '/women',
-            templateUrl: 'women.html'
-        })
-        .state('register', {
+            templateUrl: 'women.html',
+            controller: 'WomenController'
+        });
+    $stateProvider   .state('register', {
             url: '/register',
             templateUrl: 'register.html'
-        })
-        .state('checkout', {
+        });
+    $stateProvider   .state('checkout', {
             url: '/checkout',
             templateUrl: 'checkout.html'
         })
-        .state('details', {
+    $stateProvider.state('details', {
             url: '/details',
-            templateUrl: 'details.html'
-        })
-        .state('contact', {
+            templateUrl: 'details.html',
+
+        });
+    $stateProvider   .state('contact', {
             url: '/contact',
             templateUrl: 'contact.html'
         })
 });
+
+routerApp.controller('WomenController',['$scope', '$http', function ($scope, $http) {
+
+}]);
+
+routerApp.controller('DetailController',['$scope', '$http', function ($scope, $http) {
+
+}]);
