36d5b10a00d013da4ef597057cb63d4fe511f8a6
AnhTai1995 <anhtai.hcmus95@gmail.com>
2017-01-02 10:46:28 +0700

    API get mat hang theo tung loai do: Nam, Nu, So mi dai tay, ngan tay

--- API/Api_Server_SHOP/Api_Server_SHOP/Controllers/APIController.cs
+++ API/Api_Server_SHOP/Api_Server_SHOP/Controllers/APIController.cs
@@ -63,5 +63,128 @@ namespace Api_Server_SHOP.Controllers
         }
         #endregion
 
+        #region + Đồ nam
+        //Lấy về danh sách mẫu đồ nam theo 2 loại: quần hoặc áo
+        [Route("mathangnam")]
+        [HttpGet]
+        public List<MH_GioiTinh> get_MH_Nam_Theo_Loai(string Loai)
+        {
+
+            var item = (from u in db.CHITIETMATHANGs
+                        join i in db.MATHANGs on u.IDMatHang equals i.ID
+                        join m in db.LOAIHANGs on i.IDLoaiHang equals m.ID
+                        where (u.Loai == "NAM" && m.TenLoai == Loai)
+                        select new MH_GioiTinh
+                        {
+                            ID = i.ID,
+                            TenMH = i.TenMH,
+                            Mau1 = u.MauSac,
+                            URLHinhAnh1 = i.URLHinhAnh1,
+                            Gia1 = u.Gia.ToString(),
+
+                        }).ToList();
+            return item;
+        }
+
+
+        //Lấy về danh sách mẫu đồ nam theo chi tiết từng loại: quần hoặc áo => sơ mi tay dài, ngắn, quần kaki
+        [Route("mathangnamct")]
+        [HttpGet]
+        public List<MH_GioiTinh> get_MH_Nam_Theo_Loai_Chi_Tiet(string Loai, string Chitiet)
+        {
+
+            var item = (from u in db.CHITIETMATHANGs
+                        join i in db.MATHANGs on u.IDMatHang equals i.ID
+                        join m in db.LOAIHANGs on i.IDLoaiHang equals m.ID
+                        join s in db.SUBLOAIHANGs on i.IDSubLoaiHang equals s.ID
+                        where (u.Loai == "NAM" && m.TenLoai == Loai && s.TenLoai == Chitiet)
+                        select new MH_GioiTinh
+                        {
+                            ID = i.ID,
+                            TenMH = i.TenMH,
+                            Mau1 = u.MauSac,
+                            URLHinhAnh1 = i.URLHinhAnh1,
+                            Gia1 = u.Gia.ToString(),
+
+                        }).ToList();
+            return item;
+        }
+        #endregion
+
+        #region +Đồ nữ
+        //Lấy về danh sách mẫu đồ nữ theo 2 loại: quần hoặc áo
+        [Route("mathangnu")]
+        [HttpGet]
+        public List<MH_GioiTinh> get_MH_Nu_Theo_Loai(string Loai)
+        {
+
+            var item = (from u in db.CHITIETMATHANGs
+                        join i in db.MATHANGs on u.IDMatHang equals i.ID
+                        join m in db.LOAIHANGs on i.IDLoaiHang equals m.ID
+                        where (u.Loai != "NAM" && m.TenLoai == Loai)
+                        select new MH_GioiTinh
+                        {
+                            ID = i.ID,
+                            TenMH = i.TenMH,
+                            Mau1 = u.MauSac,
+                            URLHinhAnh1 = i.URLHinhAnh1,
+                            Gia1 = u.Gia.ToString(),
+
+                        }).ToList();
+            return item;
+        }
+
+        //Lấy về danh sách mẫu đồ nữ theo chi tiết từng loại: quần hoặc áo => sơ mi tay dài, ngắn, quần kaki
+        [Route("mathangnuct")]
+        [HttpGet]
+        public List<MH_GioiTinh> get_MH_Nu_Theo_Loai_Chi_Tiet(string Loai, string Chitiet)
+        {
+
+            var item = (from u in db.CHITIETMATHANGs
+                        join i in db.MATHANGs on u.IDMatHang equals i.ID
+                        join m in db.LOAIHANGs on i.IDLoaiHang equals m.ID
+                        join s in db.SUBLOAIHANGs on i.IDSubLoaiHang equals s.ID
+                        where (u.Loai != "NAM" && m.TenLoai == Loai && s.TenLoai == Chitiet)
+                        select new MH_GioiTinh
+                        {
+                            ID = i.ID,
+                            TenMH = i.TenMH,
+                            Mau1 = u.MauSac,
+                            URLHinhAnh1 = i.URLHinhAnh1,
+                            Gia1 = u.Gia.ToString(),
+
+                        }).ToList();
+            return item;
+        }
+        #endregion
+
+        #region + Ví dụ post 1 mặt hàng
+        //    [ResponseType(typeof(MATHANG))]
+        [Route("mathang")]
+        [HttpPost]
+        public bool postMatHang(string str)
+        {
+            MATHANG a = new MATHANG();
+            a.ID = "";
+            a.IDLoaiHang = "";
+            a.IDSubLoaiHang = "";
+            a.TenMH = "";
+            a.URLHinhAnh1 = "";
+            a.URLHinhAnh2 = "";
+            a.URLHinhAnh3 = "";
+            try
+            {
+                db.MATHANGs.Add(a);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
     }
 }
