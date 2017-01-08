7a597242d00c9c6c04552336be8068284ed02dc2
NguyenQuoc <nguyenx2quoc@gmail.com>
2017-01-08 19:51:18 +0700

    Delete files
    Signed-off-by: NguyenQuoc <nguyenx2quoc@gmail.com>

--- Client-Admin/js/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by asus on 1/6/2017.
- */
-photoAlbumApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
-    cloudinaryProvider
-        .set("shoponlineimage", "CCCCCCC")
-        .set("bfa8vkr6", "UUUUUUUU");
-}]);
-
